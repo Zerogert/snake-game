575b9021e8a9c39a0d2499b7a95a8f6fa02d10a9
Zlodey <36670826+Zerogert@users.noreply.github.com>
2023-03-01 16:35:45 +0700

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import pygame_gui
+
+pygame.init()
+
+size = width, height = 320, 240
+pygame.display.set_caption('Snake game')
+screen = pygame.display.set_mode(size)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
